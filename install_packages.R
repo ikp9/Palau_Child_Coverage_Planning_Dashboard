fe31312060ae0cb68617894090dfde73d1285ee1
Tippins <ikp9@cdc.gov>
2026-08-20 15:09:58 -0400

    manifest json

--- install_packages.R
+++ install_packages.R
@@ -3,7 +3,14 @@ packages <- c(
   "DT", "leaflet", "plotly", "here", "readxl", "tibble",
   "rio", "janitor", "lubridate", "purrr", "ggplot2", "sf",
   "htmltools", "htmlwidgets", "crosstalk", "RColorBrewer",
-  "openxlsx", "writexl", "knitr", "kableExtra", "ggtext"
+  "openxlsx", "writexl", "knitr", "kableExtra", "ggtext", "rsconnect"
 )
 new <- packages[!packages %in% rownames(installed.packages())]
 if (length(new)) install.packages(new)
+
+#Code below was only for publishing - does not need to be run every time
+
+install.packages(c("openssl", "rsconnect"), 
+                 repos = "https://packagemanager.posit.co/cran/latest")
+
+rsconnect::writeManifest(contentCategory = "site")
